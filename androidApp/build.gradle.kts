1377c3cfee1fab3baf180b98367cf5ba8b58adac
Szymon Klimek <szymon2klimek@gmail.com>
2022-08-22 22:27:37 +0200

    #2 Prepare simple kotlin multiplatform app
    This commit brings basic structure of client application that consists of 3 parts:
    - app: module containing domain and business logic that is shared between iOS and Android
    - androidApp: module containing Android app implementation
    - iosApp: project with iOS app implementation
    
    At the moment the application just shows simple text, it will be replaced in next commits.

---
+++ androidApp/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+}
+
+android {
+    compileSdk = 32
+    defaultConfig {
+        applicationId = "com.langsapp.android"
+        minSdk = 23
+        targetSdk = 32
+        versionCode = 1
+        versionName = "1.0"
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.3.0"
+    }
+}
+
+dependencies {
+    implementation(project(":app"))
+    implementation("androidx.core:core-ktx:1.8.0")
+    implementation("androidx.compose.ui:ui:1.2.1")
+    implementation("androidx.compose.material:material:1.2.1")
+    implementation("androidx.compose.material:material-icons-extended:1.2.1")
+    implementation("androidx.activity:activity-compose:1.5.1")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+}
---
+++ app/src/commonTest/kotlin/com/langsapp/GreetingTest.kt
@@ -0,0 +1,12 @@
+package com.langsapp
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class GreetingTest {
+
+    @Test
+    fun testGreeting() {
+        assertEquals("Hello!", Greeting().greeting())
+    }
+}
