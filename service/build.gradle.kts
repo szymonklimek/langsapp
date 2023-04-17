08be8dc2c11c3d6f6db38482ebb44e2634878367
Szymon Klimek <szymon2klimek@gmail.com>
2023-04-17 22:52:37 +0200

    #10 Add Gradle plugin dependencies
    This adds plugins necessary build Spring Boot applications that makes use of Open API generated code and are compiled to native image to be run on GraalVM.

--- service/build.gradle.kts
+++ service/build.gradle.kts
@@ -5,3 +5,15 @@ buildscript {
     )
     project.extra.set("package.group", "com.klimek.langsapp")
 }
+
+plugins {
+    kotlin("jvm") version "1.7.22" apply false
+    kotlin("plugin.spring") version "1.7.22" apply false
+
+    id("org.springframework.boot") version "3.0.5" apply false
+    id("io.spring.dependency-management") version "1.1.0" apply false
+
+    id("org.graalvm.buildtools.native") version "0.9.20" apply false
+
+    id("org.openapi.generator") version "6.4.0" apply false
+}
--- service/settings.gradle.kts
+++ service/settings.gradle.kts
@@ -3,3 +3,9 @@ rootProject.name = "langsapp"
 include(":langsapp")
 include(":users-commands")
 include(":user-follow-commands")
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
