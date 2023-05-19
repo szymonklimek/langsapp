86a6d98cc3a20dd429d590bd2bc8e951f5ed9711
Szymon Klimek <szymon2klimek@gmail.com>
2023-05-20 00:41:09 +0200

    Update Spring Boot plugin to 3.1.0

--- service/build.gradle.kts
+++ service/build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
     kotlin("jvm") version "1.7.22" apply false
     kotlin("plugin.spring") version "1.7.22" apply false
 
-    id("org.springframework.boot") version "3.0.5" apply false
+    id("org.springframework.boot") version "3.2.0" apply false
     id("io.spring.dependency-management") version "1.1.0" apply false
 
     id("org.graalvm.buildtools.native") version "0.9.20" apply false
