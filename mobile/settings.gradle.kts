000321a79bb5fc3f3bc70e04c8a90deca8f740b3
szymon-klimek <szymon2klimek@gmail.com>
2024-10-09 16:52:46 +0200

    Load all project custom gradle properties during initialization
    This commit adds reading additional ".properties" files as part of the project in the following way:
    1. Properties are stored in "Gradle" instance, which is available at any time (allowing to read them also in initialization phase)
    2. Properties are loaded as the very first thing in the build (again allowing to read them when configuring "pluginManagement" block)
    
    The changes have the following benefit: any credentials can be stores locally in ".properties" files and they are automatically added as Gradle properties and can be referenced in build files.

--- mobile/settings.gradle.kts
+++ mobile/settings.gradle.kts
@@ -1,4 +1,11 @@
 pluginManagement {
+    java.util.Properties()
+        .apply {
+            rootDir
+                .listFiles { file -> file.extension == "properties" }
+                ?.map { if (it.exists()) load(it.inputStream()) }
+        }
+        .forEach { key, value -> gradle.extra.set(key.toString(), value) }
     gradle.extra.set(
         "scm.commit.hash",
         Runtime.getRuntime().exec("git rev-parse --verify --short HEAD").apply { waitFor() }
